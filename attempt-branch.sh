b27d1588a3a9d22b598350578dcfaa00c91a1f71
Noam Lewis <sinelaw@users.noreply.github.com>
2015-10-22 22:45:07 +0300

    don't reject if push/fetch stuff fails, just leave it in proposed

--- attempt-branch.sh
+++ attempt-branch.sh
@@ -7,16 +7,19 @@ REJECT_BRANCH_PREFIX="sling/rejected/"
 STAGING="staging"
 BRANCH_NAME=$(echo "$SOURCE_BRANCH_NAME" | sed -e "s,^$SOURCE_BRANCH_PREFIX,,g")
 
+abort() {
+    # Go back to staging otherwise branch -d might fail.
+    git reset --hard
+    git checkout staging
+    git branch -D "${SOURCE_BRANCH_NAME}"
+}
 reject() {
     echo "rejecting: $BRANCH_NAME"
     (git checkout -b "${REJECT_BRANCH_PREFIX}${BRANCH_NAME}" && \
             git push -u origin "${REJECT_BRANCH_PREFIX}${BRANCH_NAME}") \
         || echo "Not overriding existing rejected branch"
-    # Go back to staging otherwise branch -d might fail.
-    git reset --hard
-    git checkout staging
-    git branch -D "${SOURCE_BRANCH_NAME}"
     git push --delete origin "${SOURCE_BRANCH_NAME}"
+    abort
     exit 1
 }
 
@@ -26,20 +29,24 @@ rebase_failed() {
     exit 1
 }
 
-trap "reject" EXIT
+trap "abort" EXIT
 
 git fetch
 git reset --hard
 git checkout $SOURCE_BRANCH_NAME
+
+
+trap "reject" EXIT
+
 git rebase origin/staging || rebase_failed
 git checkout staging
 git merge --no-ff $SOURCE_BRANCH_NAME
 
 $COMMAND
 
-git push
+trap "abort" EXIT
 
-trap - EXIT
+git push
 
 git branch -D "${SOURCE_BRANCH_NAME}"
 git push --delete origin "${SOURCE_BRANCH_NAME}"
