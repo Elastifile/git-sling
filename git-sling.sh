fd9f4a0aa682c16abd6924348e4e84473208b3a1
Noam Lewis <jones.noamle@gmail.com>
2015-10-22 15:35:48 +0300

    initial

---
+++ git-sling.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux
+
+SOURCE_BRANCH_PREFIX="sling/$1"
+TARGET_BRANCH_PREFIX="sling/$2"
+COMMAND="$3"
+
+SOURCE_DIR=$(dirname $BASH_SOURCE)
+
+git fetch
+
+git branch -r | \
+    sed -e 's,^ *origin/\(.*\),\1,g' | \
+    grep "^$SOURCE_BRANCH_PREFIX.*" | \
+    xargs -r -n1  -t $SOURCE_DIR/attempt-branch.sh $SOURCE_BRANCH_PREFIX $TARGET_BRANCH_PREFIX "$COMMAND"
+
