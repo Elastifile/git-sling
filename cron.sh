ffcf5fafe17d0d368ae514cd6047244eb0d452de
Noam Lewis <noaml@elastifile.com>
2015-10-25 09:18:27 +0200

    Added cron.sh

---
+++ cron.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eu
+WORKDIR="$1"
+COMMAND="$2"
+LOCKFILE="$HOME/sling.lock"
+
+abort() {
+   echo "Aborting."
+   exit 1
+}
+
+[ -e $LOCKFILE ] && abort
+
+trap "rm -f $LOCKFILE; exit 1" EXIT
+touch $LOCKFILE
+cd "$1" 
+~/repos/git-sling/git-sling.sh proposed/ "$COMMAND"
+
