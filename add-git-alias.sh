4fc27988d42a8a1ea021ccafaf1f2f2de294b764
Noam Lewis <jones.noamle@gmail.com>
2015-11-25 15:37:30 +0200

    add delete-rejected

--- add-git-alias.sh
+++ add-git-alias.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -eu
 SCRIPT_DIR=$(dirname $(realpath $BASH_SOURCE))
 git config --global alias.propose \!\""$SCRIPT_DIR/git-propose.sh"\"
+git config --global alias.unpropose \
+    '!'"$SCRIPT_DIR"'/git-delete-sling-branch.sh proposed'
+git config --global alias.delete-rejected \
+    '!'"$SCRIPT_DIR"'/git-delete-sling-branch.sh rejected'
---
+++ git-delete-sling-branch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+if [ "$#" -eq 1 ]; then
+    echo "Please specify branch pattern to search for (e.g. 'my_feature_branch')"
+    exit 1
+fi
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <proposed|rejected> <branch_pattern>"
+    exit 1
+fi
+PREFIX="$1"
+BRANCH="$2"
+PATTERN="sling/$PREFIX/.*$BRANCH.*"
+printf "Fetching..."
+git fetch -q
+printf "\rPruning..."
+git remote prune origin
+echo -e "\rSearching for branches to delete by pattern: \"$PATTERN\"..."
+git branch -r | grep sling | grep -o "$PATTERN" \
+    | xargs -r -n1 --interactive git push --delete origin
+echo "Done."
