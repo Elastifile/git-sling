dbb4c8c7b93cdd86e9c38e8295efa293a4d59ab9
Gosha Frost <gosha.frost@elastifile.com>
2015-11-26 14:56:50 +0200

    mount tmpfs + fix in cron

--- sling-build-cron.sh
+++ sling-build-cron.sh
@@ -4,4 +4,4 @@ ssh-add ~/.ssh/prepush_id_rsa
 SLING_DIR=~/git/git-sling
 test -d /mnt/tmpfs/example-project || (cd /mnt/tmpfs && git clone git@github.com:Elastifile/example-project.git)
 cd $SLING_DIR
-./cron.sh /mnt/tmpfs/example-project "~/git/git-sling/sling-build-example-project.sh"  &>> /tmp/build_example-project.log
+./cron.sh /mnt/tmpfs/example-project "$SLING_DIR/sling-build-example-project.sh"  &>> /tmp/build_example-project.log
