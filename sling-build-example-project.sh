cef0c68e6740f85ba99943b44f37fcf964f74962
Noam Lewis <jones.noamle@gmail.com>
2015-12-03 10:23:56 +0200

    build all commits, not just head, and disable rpm builds

--- sling-build-example-project.sh
+++ sling-build-example-project.sh
@@ -1,6 +1,22 @@
 #!/bin/bash -eux
+BASE_COMMIT="$1"
+HEAD_COMMIT="$2"
+
 git submodule update --init
-buildsome -j8 buildonly --overwrite
-buildsome -j1 --overwrite
-cd packaging
-./build
+
+build() {
+    buildsome -j16 buildonly --overwrite "$@"
+    buildsome -j4 --overwrite "$@"
+}
+
+GIT_SEQUENCE_EDITOR=true git rebase -i $BASE_COMMIT \
+                   --exec 'buildsome -j16 buildonly --overwrite && buildsome -j4 --overwrite'
+
+build --with stack_analysis
+./testing/sanity/run.sh default
+build --without optimization
+buildsome -j16 buildonly --overwrite --with clang
+
+# TODO mock build rpm
+# cd packaging
+# ./build
