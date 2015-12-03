b5b3159ec41433229f7a61feabb90d4bf8ef5334
Noam Lewis <jones.noamle@gmail.com>
2015-12-03 10:49:08 +0200

    save/restore buildsome workdir when building with different configs

--- sling-build-elfs.sh
+++ sling-build-elfs.sh
@@ -1,21 +1,50 @@
 #!/bin/bash -eux
 BASE_COMMIT="$1"
 HEAD_COMMIT="$2"
+WORKDIR_BASE="/tmp/workdir"
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+mkdir -p $WORKDIR_BASE
 
 git submodule update --init
+
+abort() {
+    cd $GIT_ROOT
+    exit 1
+}
+
+with_workdir() {
+    NAME="$1"
+    cd $WORKDIR_BASE
+    mkdir -p $NAME
+    cd $NAME
+    test -d .git || git clone $GIT_ROOT .
+    git fetch
+    git reset --hard $HEAD_COMMIT
+}
 
 build() {
     buildsome -j16 buildonly --overwrite "$@"
     buildsome -j4 --overwrite "$@"
 }
 
-GIT_SEQUENCE_EDITOR=true git rebase -i $BASE_COMMIT \
-                   --exec 'buildsome -j16 buildonly --overwrite && buildsome -j4 --overwrite'
+trap "abort" EXIT
 
+GIT_SEQUENCE_EDITOR=true git rebase -i $BASE_COMMIT \
+                   --exec 'buildsome -j8 buildonly --overwrite && buildsome -j4 --overwrite' \
+    || (git rebase --abort ; exit 1)
+
+# stack_analysis is partially incremental over regular build (only linkage)
 build --with stack_analysis
 ./testing/sanity/run.sh default
+
+with_workdir "debug"
 build --without optimization
+
+with_workdir "clang"
 buildsome -j16 buildonly --overwrite --with clang
+
+trap "" EXIT
 
 # TODO mock build rpm
 # cd packaging
