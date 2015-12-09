1c7d9bc55a570556b91e62fbe1a0d55bc2154e74
Noam Lewis <jones.noamle@gmail.com>
2015-12-09 18:29:02 +0200

    use ./tools/prepush.sh if it exists when building elfs

--- sling-build-elfs.sh
+++ sling-build-elfs.sh
@@ -2,9 +2,9 @@
 BASE_COMMIT="$(git log --format='%H' -1 $1)"
 HEAD_COMMIT="$(git log --format='%H' -1 $2)"
 SLING_DIR="$3"
-if [[ -x ./prepush.sh ]]
+if [[ -x ./tools/prepush.sh ]]
 then
-    ./prepush.sh "$@"
+    ./tools/prepush.sh "$@"
 else
     $SLING_DIR/elfs-prepush.sh "$@"
 fi
