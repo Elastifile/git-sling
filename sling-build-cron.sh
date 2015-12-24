514e5405949554ac190cce56473b7435b350754c
Eli Weissbrem <eli.weissbrem@elastifile.com>
2015-12-24 18:07:52 +0200

    change workdir to /build-workdir/elfs-system/elfs to support elfs as submodule

--- sling-build-cron.sh
+++ sling-build-cron.sh
@@ -5,4 +5,4 @@ SLING_DIR=~/git/git-sling
 # REVIEW(Eliw): this should also be protected by runonce or something like that
 #test -d /build-workdir/elfs || (cd /build-workdir && git clone git@github.com:Elastifile/elfs.git)
 cd $SLING_DIR
-./cron.sh /build-workdir/elfs "$SLING_DIR/sling-build-elfs.sh"  &>> /tmp/build_elfs.log
+./cron.sh /build-workdir/elfs-system/elfs "$SLING_DIR/sling-build-elfs.sh"  &>> /tmp/build_elfs.log
