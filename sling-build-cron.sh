3b845411bd8d6b071e9cb4aff0715eb3bdc10a6d
Eli Weissbrem <eli.weissbrem@elastifile.com>
2015-12-24 18:07:37 +0200

    remove clone of repo

--- sling-build-cron.sh
+++ sling-build-cron.sh
@@ -2,6 +2,7 @@
 ssh-add -D
 ssh-add ~/.ssh/prepush_id_rsa
 SLING_DIR=~/git/git-sling
-test -d /build-workdir/elfs || (cd /build-workdir && git clone git@github.com:Elastifile/elfs.git)
+# REVIEW(Eliw): this should also be protected by runonce or something like that
+#test -d /build-workdir/elfs || (cd /build-workdir && git clone git@github.com:Elastifile/elfs.git)
 cd $SLING_DIR
 ./cron.sh /build-workdir/elfs "$SLING_DIR/sling-build-elfs.sh"  &>> /tmp/build_elfs.log
