ee6f13f1523a4493ec8ca0536e534333850161d4
Eli Weissbrem <eli.weissbrem@elastifile.com>
2015-12-24 18:07:21 +0200

    remove support for local prepush script

--- sling-build-elfs.sh
+++ sling-build-elfs.sh
@@ -2,9 +2,4 @@
 BASE_COMMIT="$(git log --format='%H' -1 $1)"
 HEAD_COMMIT="$(git log --format='%H' -1 $2)"
 SLING_DIR="$3"
-if [[ -x ./tools/prepush.sh ]]
-then
-    ./tools/prepush.sh "$BASE_COMMIT" "$HEAD_COMMIT"
-else
-    $SLING_DIR/elfs-prepush.sh "$@"
-fi
+./tools/prepush.sh "$BASE_COMMIT" "$HEAD_COMMIT"
