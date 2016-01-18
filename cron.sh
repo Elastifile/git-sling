049c27d81a373dfba6693bbab998fdad6999a8d0
Noam Lewis <jones.noamle@gmail.com>
2016-01-18 18:55:36 +0200

    cron: put entire script under flock; move .stack to workdir

--- cron.sh
+++ cron.sh
@@ -4,11 +4,18 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 source $SCRIPT_DIR/sling-config.sh
 
-cd $SCRIPT_DIR/server
-stack setup
-stack build
-SLING_SERVER=$(stack path --project-root)/$(stack path --dist-dir)/build/sling-server-exe/sling-server-exe
+(
+    flock -n 9 || exit 1
 
-test -d $SERVER_WORKDIR/example-project-system || (cd $SERVER_WORKDIR && git clone git@github.com:Elastifile/example-project-system.git && git submodule update --init)
-cd $SERVER_WORKDIR/example-project-system/example-project
-flock -n "$SERVER_LOCKFILE" "$SLING_SERVER" "$COMMAND"
+    cd $SCRIPT_DIR/server
+    mkdir -p $SERVER_WORKDIR/.stack
+    ln -s $SERVER_WORKDIR/.stack ~/.stack || echo "linking .stack failed, ignoring."
+    stack setup
+    stack build
+    SLING_SERVER=$(stack path --project-root)/$(stack path --dist-dir)/build/sling-server-exe/sling-server-exe
+
+    test -d $SERVER_WORKDIR/example-project-system || (cd $SERVER_WORKDIR && git clone git@github.com:Elastifile/example-project-system.git && git submodule update --init)
+    cd $SERVER_WORKDIR/example-project-system/example-project
+    "$SLING_SERVER" "$COMMAND"
+
+) 9>$SERVER_LOCKFILE
