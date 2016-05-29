9afe239140c242836f17b0b4414fe0c622123202
Noam Lewis <noam.lewis@elastifile.com>
2016-05-29 15:41:18 +0300

    installer: parameterize working dir; move homedir to be under it

--- remote-install.sh
+++ remote-install.sh
@@ -6,6 +6,7 @@ target_host=$(echo $target | sed -r 's/([^@]+@)?(.*)/\2/g')
 keyfile=$(mktemp)
 
 sling_user="sling"
+workdir="/build-workdir"
 
 trap "rm $keyfile" EXIT
 
@@ -19,15 +20,15 @@ on_remote_as_build() {
     ssh -i $keyfile $sling_user@$target_host "$@"
 }
 
-on_remote "getent passwd $sling_user || sudo useradd $sling_user"
+on_remote "getent passwd $sling_user || sudo useradd -b $workdir $sling_user"
 on_remote "sudo su - $sling_user bash -c 'mkdir -p \$HOME/.ssh && chmod 700 \$HOME/.ssh'"
 build_homedir=$(on_remote 'getent passwd $sling_user | cut -d: -f6')
 scp $keyfile.pub $target:/tmp
 on_remote "sudo su - $sling_user bash -c 'cat /tmp/$keyfile_name.pub >> \$HOME/.ssh/authorized_keys && chmod 600 \$HOME/.ssh/authorized_keys'"
 on_remote "rm /tmp/$keyfile_name.pub"
-on_remote "sudo chown $sling_user /build-workdir"
+on_remote "sudo chown $sling_user $workdir"
 
 git_remote_url=$(git config remote.origin.url)
 
-on_remote_as_build "cd /build-workdir && ( cd git-sling && ( git stash || true ) && git fetch -p && git checkout master && git reset --hard origin/master ) || git clone $git_remote_url"
-on_remote_as_build "cd /build-workdir/git-sling && ./install-server.sh"
+on_remote_as_build "cd $workdir && ( cd git-sling && ( git stash || true ) && git fetch -p && git checkout master && git reset --hard origin/master ) || git clone $git_remote_url"
+on_remote_as_build "cd $workdir/git-sling && ./install-server.sh"
