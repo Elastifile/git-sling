3d9c0924f69584c552596267f4f722f7e7d0cdd3
Noam Lewis <noam.lewis@elastifile.com>
2017-06-22 15:29:51 +0300

    Add test for duplicate proposals

--- server/test/sanity/test.sh
+++ server/test/sanity/test.sh
@@ -95,6 +95,7 @@ exec_test() {
 
 
 exec_test "tests/basic.sh"
+exec_test "tests/duplicate-propose.sh"
 exec_test "tests/take-job.sh"
 exec_test "tests/rebase.sh"
 exec_test "tests/onto.sh"
---
+++ server/test/sanity/tests/duplicate-propose.sh
@@ -0,0 +1,54 @@
+
+echo "Testing duplicate propose"
+
+cd_client
+logit fetch -p
+
+logit checkout -b "duplicate_propose"
+logit reset --hard origin/master
+add_commit_file "double_propose_A_file1"
+add_commit_file "double_propose_A_file2"
+
+yes | run_cmd $sling_propose master
+
+cd_server
+
+echo "Expecting success..."
+run_cmd $sling_server take-job || fail "ERROR: Server should succeed!"
+
+cd_client
+
+logit fetch -p
+
+git branch -r |grep -E '/in-progress/.*duplicate_propose' || fail "Should have in-progress"
+
+add_commit_file "double_propose_A_file3"
+
+yes | run_cmd $sling_propose master
+yes | run_cmd $sling_propose master
+
+cd_server
+
+echo "Expecting success..."
+run_cmd $sling_server take-job || fail "ERROR: Server should succeed!"
+
+echo "Expecting success..."
+run_cmd $sling_server poll -- $prepush || fail "ERROR: Server should succeed!"
+
+echo "Expecting success..."
+run_cmd $sling_server poll -- $prepush || fail "ERROR: Server should succeed!"
+
+echo "Expecting success..."
+run_cmd $sling_server poll -- $prepush || fail "ERROR: Server should succeed!"
+
+cd_client
+
+logit fetch -p
+
+logit checkout master
+
+! ls double_propose* || fail "Files should not exist"
+
+logit rebase
+
+ls double_propose* || fail "Files should exist"
