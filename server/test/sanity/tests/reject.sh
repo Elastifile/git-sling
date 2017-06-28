e4ad4f2188afdc865559a3a70595b002587eaeb8
Noam Lewis <noam.lewis@elastifile.com>
2017-06-28 21:54:08 +0300

    Add missing file: test/sanity/tests/reject.sh (belongs to c9cb549bcdda588a806e1e2c398a6c8075789e53)

---
+++ server/test/sanity/tests/reject.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+logit checkout master
+logit reset --hard origin/master
+
+master_hash=$(git rev-parse origin/master)
+
+logit checkout -b reject_me
+add_commit_file reject_me
+
+yes | run_cmd $sling_propose master
+
+cd_server
+
+tmp_stdout=$(mktemp)
+
+$sling_server take-job --match-branches 'master' > $tmp_stdout
+proposal_branch_name=$(sed -n 1p $tmp_stdout)
+
+rm $tmp_stdout
+
+echo "rejecting: $proposal_branch_name"
+
+$sling_server reject $proposal_branch_name "it literally stinks"
+
+# ----------------------------------------------------------------------
+cd_client
+
+logit fetch -p
+
+git branch -r | grep '/rejected/.*reject_me' || fail "Expected branch to be rejected"
